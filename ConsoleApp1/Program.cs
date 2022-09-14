f598849fe0e29a213239d40dc6b28fcb700b8ec4
Andrew Seavers <17SeaveA@fulford.york.sch.uk>
2022-09-14 12:17:12 +0100

    Made lots of changes to the code and improving functionality

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,49 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string pizza = Input("What kind of pizza would you like? ");
+
+            pizza = pizza.ToLower();
+
+            if (pizza == "cheese and tomato" || pizza == "margherita")
+            {
+                Console.WriteLine("Suitable for vegetarians");
+            }
+            else if (pizza == "pepperoni" || pizza == "meat feast")
+            {
+                Console.WriteLine("Not suitable for vegetarians");
+            }
+            else
+            {
+                Console.WriteLine("Unknown pizza. May contain meat!");
+            }
+            Console.ReadLine();
+
+            pizza = Input("What type of pizza would you like? ");
+
+            pizza = pizza.ToLower();
+
+            switch(pizza)
+            {
+                case "cheese and tomato":
+                case "margherita":
+                    Console.WriteLine("Suitable for vegetarians");
+                    break;
+                case "meat feast":
+                case "pepperoni":
+                    Console.WriteLine("Not suitable for vegetarians");
+                    break;
+                default:
+                    Console.WriteLine("Pizza unknown, may contain meat!");
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+        private static string Input(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
         }
     }
 }
